974237265d23f7c21d57f6fc5a6d3a00ed60b166
Leonardo Souza <leonrevolution2077@hotmail.com>
2025-06-09 20:56:07 -0300

    xadrezBasico.c

---
+++ xadrezBasico.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    // Movimento da Torre (5 casas para a direita) usando FOR
+    int movimentoTorre = 5;
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < movimentoTorre; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
+    // Movimento do Bispo (5 casas na diagonal para cima e à direita) usando WHILE
+    int movimentoBispo = 5;
+    int i = 0;
+    printf("Movimento do Bispo:\n");
+    while (i < movimentoBispo) {
+        printf("Cima Direita\n");
+        i++;
+    }
+
+    printf("\n");
+
+    // Movimento da Rainha (8 casas para a esquerda) usando DO-WHILE
+    int movimentoRainha = 8;
+    int j = 0;
+    printf("Movimento da Rainha:\n");
+    do {
+        printf("Esquerda\n");
+        j++;
+    } while (j < movimentoRainha);
+
+    return 0;
+}
